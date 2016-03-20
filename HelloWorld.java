7a92c7f49daa73e3a96f1233461e03cfad7c51bf
JB <jb.viboud@hotmail.fr>
2016-03-20 22:53:28 +0100

    ajout de quelques commentaires

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
+/**
+* Main class : affiche Hello, World
+* author : JB
+*/
 public class HelloWorld {
-
+	//main methode
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
